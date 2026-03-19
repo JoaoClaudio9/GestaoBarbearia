009967e150cb55aba54217fc57de1230eff8688a
JoaoClaudio9 <jpereiradesouzaneto1@gmail.com>
2026-03-19 14:43:48 -0300

    criação da classe servico

---
+++ src/gestaobarbearia/servico.java
@@ -0,0 +1,40 @@
+
+package gestaobarbearia;
+
+abstract class Servico {
+    
+    private String cliente;
+    private String descricao;
+    private double precoBase;
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setPrecoBase(double precoBase) {
+        this.precoBase = precoBase;
+    }
+
+    public void calcularPreco(){
+        
+    }
+        
+}
+
+
